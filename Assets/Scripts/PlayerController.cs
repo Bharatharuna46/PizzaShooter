63d5466b1e6a1fad61b5e889f74244a42fdbecb7
Bharatharuna46 <bharatharuna46@gmail.com>
2020-12-16 18:53:18 +0530

    Version1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public int moveSpeed;
+    public float xRange;
+
+    public GameObject foodPrefab;
+    //private Quaternion foodrotation = Quaternion.Euler(0,0,60);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed *horizontalInput);
+
+        Boundary();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodPrefab, transform.position, foodPrefab.transform.rotation);
+        }
+    }
+    void Boundary()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
